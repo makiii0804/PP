e73c8fef49736347f040b30da9e09d9c01a595cc
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-09 21:23:37 +0200

    ES6 syntax shopping list exercise FINISHED

--- ES6/shopping-list-exercise-ES6/index.js
+++ ES6/shopping-list-exercise-ES6/index.js
@@ -51,3 +51,48 @@ function ShoppingBag() {
     return `Total price: ${sum.toFixed(3)}`;
   };
 }
+function PaymentCard(accBalance, cardStatus, validDate) {
+    if (!accBalance || !cardStatus || !validDate) {
+      throw new Error("This field is required!");
+    }
+    this.accBalance = accBalance.toFixed(2);
+    this.cardStatus = cardStatus;
+    this.validDate = new Date(validDate);
+    if (!(this.validDate - new Date() > 0)) {
+      throw new Error("This credit card is expired.");
+    }
+    if (this.cardStatus !== "active") {
+      throw new Error("This credit card is inactive.");
+    }
+  }
+  function checkOutAndBuy(shoppingBag, paymentCard) {
+    if (!shoppingBag || !(shoppingBag instanceof ShoppingBag)) {
+      throw new Error("Invalid input.");
+    }
+    if (!paymentCard || !(paymentCard instanceof PaymentCard)) {
+      throw new Error("Invalid input.");
+    }
+    if (parseFloat(paymentCard.accBalance) >= shoppingBag.calculateTotalPrice()) {
+      return `Purchase is successiful.`;
+    } else {
+      return `Purchase is not successiful! Missing amount: ${
+        shoppingBag.calculateTotalPrice() - paymentCard.accBalance
+      }`;
+    }
+  }
+  let product1 = new Product("Banana", 100, "12 Oct 2021");
+  let product2 = new Product("Apple", 200, "12 Nov 2020");
+  let product3 = new Product("Avocado", 300, "17 Jul 2021");
+  let card = new PaymentCard(200, "active", "30 Dec 2021");
+  console.log(product1.getInfo());
+  console.log(product2.getInfo());
+  console.log(product3.getInfo());
+  let bag = new ShoppingBag();
+  bag.addProduct(product1);
+  bag.addProduct(product2);
+  bag.addProduct(product3);
+  console.log(bag.listOfProducts);
+  console.log(bag.getAverage());
+  console.log(bag.getMostExpensive());
+  console.log(bag.calculateTotalPrice());
+  console.log(checkOutAndBuy(bag, card));
