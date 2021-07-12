3ada195a2efb38eae2d008835985452a70adbd87
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-12 22:53:09 +0200

    ES6 practicals task 6 FINISHED

--- ES6/ES6-practicals-initial/index.js
+++ ES6/ES6-practicals-initial/index.js
@@ -64,6 +64,10 @@ console.log(output(items));
 Input: [1.6, 11.34, 9.23, 7, 3.11, 8]
 Output: [7, 8]*/
 
+const str = [1.6, 11.34, 9.23, 7, 3.11, 8];
+const filteredNumbers = num => num.filter(item=> parseInt(item) == item).map(item=>parseInt(item));
+console.log(filteredNumbers(str));
+
 /*7.	Write a function that filters all integer arguments that contain digit 5.
 Function arguments: 23, 11.5, 9, 'abc', 45, 28, 553 
 Output: [45, 553]*/
